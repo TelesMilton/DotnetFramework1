02ae32cd2be6636c3fde68ac4eb2d84cb7f73dec
Milton <telesmilton1000@gmail.com>
2021-07-31 01:10:08 -0300

    Subindo arquivo

---
+++ DotnetFramework1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetFramework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int num1, num2, soma;
+			Console.WriteLine("digite um numero");
+			num1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("digite um numero");
+			num2 = int.Parse(Console.ReadLine());
+			soma = num1 + num2;
+			Console.WriteLine("a soma é: " + soma);
+			soma = num1 * num2;
+			Console.WriteLine("a mult é: " + soma);
+			soma = num1 / num2;
+			Console.WriteLine("a div é: " + soma);
+			soma = num1 - num2;
+			Console.WriteLine("a sub é: " + soma);
+		}
+    }
+}
